f634f47d55aaa36883ec2493ef48d7a7f8c2f91d
Nishantaryan <Nishantawesom@hotmail.com>
2020-08-05 14:56:26 +0700

    changes in input data

--- App.js
+++ App.js
@@ -1,60 +1,59 @@
 import React, { useState } from 'react';
 import { Text, View,TextInput,Button,StyleSheet,FlatList } from 'react-native';
-import GoalList from './components/GoalList';
+
  
 
 const App = () => {
  
-  
-const [goalsItems,variableForHoldingUserText] = useState('');
-const [goalsArray,goalsEmptyArray] = useState([]);
+  const [setText,getText] = useState('');
+  const [goalsArray,UserGoals] = useState([]);
+  const getUserText = (text) => {
+    getText(text);
+  }      
 
-  const getUserInput = (text) => {
-    variableForHoldingUserText(text);
+  const getUserGoals = () => {
+    UserGoals(currentGoals => [...currentGoals,{key : Math.random().toString() , value : setText}]) ;
+
   }
+   
+  
 
-  const onButtonTouch = () => {
-     goalsEmptyArray(existingGoal =>  [...existingGoal, { id : Math.random().toString()  , value : goalsItems  }
-    ] );
-    };
+  const showUserInput = () => {
+    console.log(setText);
+  }
   
   
 
   return (
-       <View style = {styles.container} >
-           <View>
-               <TextInput onChangeText = {getUserInput} style = {styles.textInput}/>
-               <Button title = "Add" onPress={onButtonTouch} />
-           </View>
-
-          <FlatList 
-                   style = {styles.listItem} 
-                   data = {goalsArray} 
-                   keyExtractor = { (item,index) => item.id} 
-                   renderItem={itemIterator => (
-                     <GoalList title = {itemIterator.item.value}/>
-          )} />
-        
+       <View style = {styles.container}>
+         <View>
+           <TextInput style={styles.inputText} onChangeText = {getUserText} />
+           <Button title = "Add" onPress = {getUserGoals} />
+         </View>
+         <View>
+            <FlatList keyExtractor= {(item,index) => item.key} 
+                      data = {goalsArray}
+                      renderItem = {itemIterator => 
+                      <Text style = {styles.listStyle}>{itemIterator.item.value  }</Text> }  />
+         </View>
        </View>
 
   );
   
           }
-  const styles = StyleSheet.create({
-    container : {
-      padding : 50
-    },
-    textInput : {
-      borderWidth : 1,
-      borderColor : 'black'
-    },
-    listItem: {
-      padding: 10,
-      marginVertical: 10,
-      backgroundColor: '#ccc',
-      borderColor: 'black',
-      borderWidth: 1
-    }
-  })
+ 
+const styles = StyleSheet.create({
+  container : {
+    padding : 20
+  },
+  inputText : {
+     borderWidth : 1
+  },
+  listStyle : {
+    padding : 20,
+    borderWidth : 1,
+     
+  }
+})          
 
 export default App;
